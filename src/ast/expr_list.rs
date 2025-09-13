5f6c6423c09a4535891dc6fbec4d54246c84ff21
SSoup64 <sahar.c2007@gmail.com>
2025-09-13 11:56:23 +0300

    feat: add ExprList ast node

---
+++ src/ast/expr_list.rs
@@ -0,0 +1,25 @@
+use super::ast_node::AstNode;
+
+pub struct ExprList {
+    stmts: Vec<Box<dyn AstNode>>,
+    tail: Box<dyn AstNode>
+}
+
+impl ExprList {
+    pub fn new(stmts: Vec<Box<dyn AstNode>>, tail: Box<dyn AstNode>) -> Self {
+        Self {
+            stmts,
+            tail
+        }
+    }
+}
+
+impl AstNode for ExprList {
+    fn evaluate(&self) {
+        for stmt in &self.stmts {
+            let _ = stmt.evaluate();
+        }
+
+        self.tail.evaluate()
+    }
+}
