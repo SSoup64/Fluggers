6928de90114bca45e806f2385add97feeabae8a0
SSoup64 <sahar.c2007@gmail.com>
2025-09-13 21:18:14 +0300

    feat: add BinOp node

---
+++ src/ast/bin_op.rs
@@ -0,0 +1,26 @@
+use super::ast_node::AstNode;
+use crate::token::Token;
+
+#[derive(Debug)]
+pub struct BinOp<'input> {
+    left: Box<dyn AstNode + 'input>,
+    right: Box<dyn AstNode + 'input>,
+    op: Token<'input>,
+}
+
+impl<'input> BinOp<'input> {
+    pub fn new(left: Box<dyn AstNode + 'input>, right: Box<dyn AstNode + 'input>, op: Token<'input>) -> Self {
+        Self {
+            left,
+            right,
+            op
+        }
+    }
+}
+
+impl<'input> AstNode for BinOp<'input> {
+    fn evaluate(&self) {
+        // TODO:
+        // Implement
+    }
+}
--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -3,7 +3,9 @@ pub mod ast_node;
 
 pub mod expr_list;
 pub mod int_literal;
+pub mod bin_op;
 
 // Import nodes from modules
 pub use expr_list::ExprList;
 pub use int_literal::IntLiteral;
+pub use bin_op::BinOp;
