461951a1d181f9c2c1f8c455ea49acba49930af1
SSoup64 <sahar.c2007@gmail.com>
2025-09-13 21:21:55 +0300

    feat: add methods to get bp, led handler and nud handler of tokens

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,7 @@
+use crate::binding_power::BindingPower;
+use crate::ast::{ast_node::AstNode, *};
+use crate::parser::Parser;
+
 #[derive(Debug, Clone)]
 pub enum Token<'input> {
     Identifier(&'input str),
@@ -47,3 +51,43 @@ pub enum Token<'input> {
 
     Seminolon,
 }
+
+impl<'input> Token<'input> {
+    pub fn get_binding_power(&self) -> Option<BindingPower> {
+        match self {
+            Token::Plus | Token::Minus => Some(BindingPower::Additive),
+            Token::Star | Token::Slash => Some(BindingPower::Multiplicative),
+            Token::ParenOpen => Some(BindingPower::Group),
+            _ => None,
+        }
+    }
+    
+    pub fn into_led_handler(self) -> Option<Box<dyn FnOnce(&mut Parser<'input>, Box<dyn AstNode + 'input>, BindingPower) -> Box<dyn AstNode + 'input> + 'input>> {
+        match self {
+            op @ (Token::Plus | Token::Minus | Token::Star | Token::Slash) => {
+                Some(Box::new(move |parser, left , bp| {
+                    let right = parser.parse_expr(bp);
+                    Box::new(BinOp::new(left, right, op))
+                }))
+            },
+            _ => None,
+        }
+    }
+
+    pub fn into_nud_handler(self) -> Option<Box<dyn FnOnce(&mut Parser) -> Box<dyn AstNode>>> {
+        match self {
+            Token::IntLiteral(val) => {
+                Some(Box::new(move |_: &mut Parser| {
+                    Box::new(IntLiteral::new(val))
+                }))
+            },
+            _ => None,
+        }
+    }
+}
+
+
+
+
+
+
