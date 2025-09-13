b146bb4f2a2058096ef7ececd20331d95f2953a0
SSoup64 <sahar.c2007@gmail.com>
2025-09-13 21:22:50 +0300

    feat: implement the rest of Parser::parse_expr

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,10 @@
 use crate::ast::{self, ast_node::AstNode};
-
 use crate::token::Token;
+use crate::binding_power::BindingPower;
 
 use std::collections::VecDeque;
+
+use phf::phf_map;
 
 pub struct Parser<'input> {
     tokens: VecDeque<Token<'input>>,
@@ -13,9 +15,8 @@ impl<'input> Parser<'input> {
         Self { tokens }
     }
 
-    pub fn into_ast(mut self) -> Result<Box<dyn ast::ast_node::AstNode>, ()> {
-        let _ = self.parse_expr_list();
-        Err(())
+    pub fn into_ast(mut self) -> Result<Box<dyn ast::ast_node::AstNode + 'input>, ()> {
+        Ok(Box::new(self.parse_expr_list()))
     }
 
     // Util funcs
@@ -32,12 +33,12 @@ impl<'input> Parser<'input> {
     }
 
     // Parsing funcs
-    fn parse_expr_list(&mut self) -> ast::ExprList {
+    fn parse_expr_list(&mut self) -> ast::ExprList<'input> {
         let mut stmts: Vec<Box<dyn AstNode>> = vec![];
         let tail: Box<dyn AstNode>;
 
         loop {
-            let expr = self.parse_expr();
+            let expr = self.parse_expr(BindingPower::Min);
 
             if let Some(Token::Seminolon) = self.cur_token() {
                 stmts.push(expr);
@@ -47,12 +48,44 @@ impl<'input> Parser<'input> {
                 break;
             }
         }
-
+        
         ast::ExprList::new(stmts, tail)
     }
+    
+    // TODO: Find a way to have parse_expr private
+    pub fn parse_expr(&mut self, min_bp: BindingPower) -> Box<dyn AstNode + 'input> {
+        let cur_token = self.consume().unwrap();
 
-    fn parse_expr(&mut self) -> Box<dyn AstNode> {
-        // Placeholder implementation
-        Box::new(ast::IntLiteral::new(1))
+        // Parse the NUD
+        let nud_handler = cur_token.into_nud_handler().expect("Expected NUD handler");
+        let mut left_hand_side: Box<dyn AstNode> = nud_handler(self);
+
+        loop {
+            // Check if we should stop the loop
+            let Some(next_token) = self.cur_token() else {
+                break;
+            };
+
+            if next_token.get_binding_power().unwrap() < min_bp {
+                break;
+            }
+
+            // Next token
+            let bp = next_token.get_binding_power().unwrap();
+            let led_handler = self.consume().unwrap().into_led_handler().expect("Expected LED handler");
+            left_hand_side = led_handler(self, left_hand_side, bp);
+        }
+        
+        left_hand_side
     }
 }
+
+
+
+
+
+
+
+
+
+
