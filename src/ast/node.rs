0d2cf2c4bbd5853d969ff2b1df9a2784580de608
SSoup64 <sahar.c2007@gmail.com>
2025-09-20 16:57:52 +0300

    feat: manually implement Debug trait for ast::node::Node to make it prettier

--- src/ast/node.rs
+++ src/ast/node.rs
@@ -1,10 +1,11 @@
+use std::fmt;
+
 use super::bin_op::BinOp;
 use super::expr_list::ExprList;
 use super::int_literal::IntLiteral;
 use super::var_decl::VarDecl;
 
 // Node enum
-#[derive(Debug)]
 pub enum Node<'input> {
     BinOp(Box<BinOp<'input>>),
     ExprList(Box<ExprList<'input>>),
@@ -19,6 +20,17 @@ impl<'input> Node<'input> {
             Node::ExprList(expr_list) => expr_list.evaluate(),
             Node::IntLiteral(int_literal) => int_literal.evaluate(),
             Node::VarDecl(var_decl) => var_decl.evaluate(),
+        }
+    }
+}
+
+impl<'input> fmt::Debug for Node<'input> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Node::BinOp(bin_op) => bin_op.fmt(f),
+            Node::ExprList(expr_list) => expr_list.fmt(f),
+            Node::IntLiteral(int_literal) => int_literal.fmt(f),
+            Node::VarDecl(var_decl) => var_decl.fmt(f),
         }
     }
 }
