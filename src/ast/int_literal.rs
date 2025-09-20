61e08e3bd0fafe7bee08aa4d09cf34862fe54781
SSoup64 <sahar.c2007@gmail.com>
2025-09-20 20:31:28 +0300

    feat: print IntLiteral on one line when pretty printing

--- src/ast/int_literal.rs
+++ src/ast/int_literal.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+use std::fmt;
+
 pub struct IntLiteral(i128);
 
 impl IntLiteral {
@@ -7,4 +8,10 @@ impl IntLiteral {
     }
 
     pub fn evaluate(&self) {}
+}
+
+impl fmt::Debug for IntLiteral {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "IntLiteral({})", self.0)
+    }
 }
